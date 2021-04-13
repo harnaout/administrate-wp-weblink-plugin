196765206faa73c1aee7ed07df93e0f65240f1c7
Jad Khater <jck@administrate.co>
2021-04-13 20:22:11 +0300

    PLAT-10154 Add Shortcode to trigger add to car modal for LPs and Course

--- public/js/admwswp-public.js
+++ public/js/admwswp-public.js
@@ -1,31 +1,18 @@
 jQuery(function($) {
-
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
-
+	$(document).ready(function () {
+		$('.admwswp-addToCart', 'body').on('click', function(){
+			if (weblink !== undefined) {
+				var pathId = $(this).data('path_id');
+				if (pathId) {
+					weblink.stores.STORE_PATH_PICKER.setPathId(pathId);
+					weblink.stores.STORE_DETAILS.openAddPathToCart();
+				}
+				var courseId = $(this).data('course_id');
+				if (courseId) {
+					weblink.stores.STORE_EVENT_PICKER.setCourseId(courseId);
+					weblink.stores.STORE_DETAILS.openAddToCart();
+				}
+			}
+		});
+	});
 });
